5e53c4e51e3959748ba35f49bb2029cc5c28a0a8
TMaster <github321>
2014-06-07 19:17:58 +0300

    Raw implementation of the ViewModel
    Still working on how to make it work

---
+++ TreeNavigation/NavigationViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace TreeNavigation
+{
+    /// <summary>
+    /// Navigation logic for WPF TreeView
+    /// </summary>
+    public class NavigationViewModel : ReactiveObject
+    {
+
+        /// <summary>
+        /// The input stream from KeyUp event
+        /// </summary>
+        private IObservable<string> mLettersStream;
+
+
+        /// <summary>
+        /// The phrase to navigate to
+        /// </summary>
+        private ObservableAsPropertyHelper<string> mNavigationPhrase;
+
+        /// <summary>
+        /// Gets the <see cref="mNavigationPhrase"/>
+        /// </summary>
+        public string NavigationPhrase
+        {
+            get { return mNavigationPhrase.Value; }
+        }
+
+        public NavigationViewModel(UIElement operationElement)
+        {
+
+            var event1 =
+                     Observable.FromEventPattern<KeyEventArgs>(operationElement, "KeyUp");
+
+            
+
+            mLettersStream = event1.Select(
+                arg => arg.EventArgs.Key.ToString().ToLower());
+
+            mLettersStream.Scan(String.Concat).ToProperty(this, vm => vm.NavigationPhrase).ObserveOn(SynchronizationContext.Current).Subscribe(_ => MessageBox.Show(NavigationPhrase));
+
+
+            
+
+        }
+
+    }
+}
